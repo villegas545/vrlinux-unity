1d0e2d1dd45e966ca9a18d6ea71c67da76a8324c
villegas545 <villegas_545@hotmail.com>
2021-06-25 19:05:52 -0600

    commit

---
+++ Assets/scrpits/IA.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class IA : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject esqueleto;
+    public float distanciaMaxima,distanciaMinima;
+    public Text puntaje;
+    private float tiempo;
+
+    void Update()
+    {
+        float distancia = Vector3.Distance(target.GetComponent<Transform>().position, esqueleto.GetComponent<Transform>().position);
+        Debug.Log(distancia);
+        if (distancia >distanciaMaxima)
+        //   8 <  5
+        {
+            //DETENER
+            esqueleto.GetComponent<Animator>().SetBool("caminar", false);
+            esqueleto.GetComponent<NavMeshAgent>().isStopped = true;
+            esqueleto.GetComponent<NavMeshAgent>().ResetPath();
+        }
+        else if(distancia <distanciaMaxima && distancia >distanciaMinima)
+        {
+            //CAMINAR
+            esqueleto.GetComponent<Animator>().SetBool("caminar", true);
+            esqueleto.GetComponent<NavMeshAgent>().SetDestination(target.GetComponent<Transform>().position);
+        }
+        else if(distancia <distanciaMinima){
+            esqueleto.GetComponent<Animator>().SetTrigger("atacar");
+            esqueleto.GetComponent<NavMeshAgent>().isStopped = true;
+            esqueleto.GetComponent<NavMeshAgent>().ResetPath();
+            if(tiempo<Time.time){
+                tiempo = Time.time + .5f;
+                float puntos = float.Parse(puntaje.text)-10;
+                puntaje.text = "" + puntos;
+            }
+        }
+    }
+}
+
+
+//diastancia sea 10< disantai 
---
+++ Assets/scrpits/gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class gamemanager : MonoBehaviour
+{
+    public Text puntaje;
+    public Text gameOver;
+    
+    void Update()
+    {
+         float puntos = float.Parse(puntaje.text);
+        if(puntos<=0){
+            gameOver.enabled = true;
+            Debug.Log("" + puntos);
+        }
+    }
+}
